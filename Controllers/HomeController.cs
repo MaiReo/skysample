77aaeb908f55ae4911a7836cc07aab5c4a1f3fb5
MaiReo <sawako.saki@gmail.com>
2018-12-01 15:02:35 +0800

    init demo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sky_sample.Models;
+
+namespace sky_sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            using (var response = await _httpClient.GetAsync("https://www.baidu.com"))
+            {
+                ViewData["BaiduStatusCode"] = response.StatusCode.ToString();
+            }
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+namespace sky_sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args)
+                //.ConfigureServices(services => AddSkyWalkingCore(services))
+                .UseStartup<Startup>();
+
+            return builder;
+        }
+
+        public static void AddSkyWalkingCore(IServiceCollection services)
+        {
+            //AddSkyWalkingCore
+            var typeName = Assembly.CreateQualifiedName("SkyWalking.Agent.AspNetCore, Culture=neutral, PublicKeyToken=null", "SkyWalking.Agent.AspNetCore.ServiceCollectionExtensions");
+            var type = Type.GetType(typeName);
+            var method = type.GetMethod("AddSkyWalkingCore", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+            method.Invoke(null, new object[] { services });
+        }
+    }
+}
